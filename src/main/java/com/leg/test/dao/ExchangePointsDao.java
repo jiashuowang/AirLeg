51d108c337286a8fade5f2a40b82ab899d600b35
jiashuowang <eternity00002@gmail.com>
2020-05-21 16:25:28 +0800

    initial commit

--- src/main/java/com/leg/test/dao/ExchangePointsDao.java
+++ src/main/java/com/leg/test/dao/ExchangePointsDao.java
@@ -4,6 +4,9 @@ import com.leg.test.domain.po.ExchangePoints;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ExchangePointsDao extends JpaRepository<ExchangePoints, String> {
+    List<ExchangePoints> findByDepctAndArrctAndDeptime(String dep_ct, String arr_ct, String dep_time);
 }
--- src/main/java/com/leg/test/domain/po/ExchangePoints.java
+++ src/main/java/com/leg/test/domain/po/ExchangePoints.java
@@ -11,82 +11,82 @@ import java.io.Serializable;
 public class ExchangePoints implements Serializable {
     @Id //主键
     @Column(name = "FLIGHT_NO")
-    private String flight_no;
+    private String flightno;
     @Column(name = "POINTS_NEED")
-    private int points_need;
+    private int pointsneed;
     @Column(name = "DEP_CT")
-    private String dep_ct;
+    private String depct;
     @Column(name = "ARR_CT")
-    private String arr_ct;
+    private String arrct;
     @Column(name = "DEP_TIME")
-    private String dep_time;
+    private String deptime;
 
     public ExchangePoints() {
     }
 
     public ExchangePoints(String flight_no, String dep_ct, String arr_ct, String dep_time){
-        this.flight_no = flight_no;
-        this.dep_ct = dep_ct;
-        this.arr_ct = arr_ct;
-        this.dep_time = dep_time;
+        this.flightno = flight_no;
+        this.depct = dep_ct;
+        this.arrct = arr_ct;
+        this.deptime = dep_time;
     }
 
     public ExchangePoints(String flight_no, int points_need, String dep_ct, String arr_ct, String dep_time){
-        this.flight_no = flight_no;
-        this.points_need = points_need;
-        this.dep_ct = dep_ct;
-        this.arr_ct = arr_ct;
-        this.dep_time = dep_time;
+        this.flightno = flight_no;
+        this.pointsneed = points_need;
+        this.depct = dep_ct;
+        this.arrct = arr_ct;
+        this.deptime = dep_time;
     }
 
     public String getFlight_no() {
-        return flight_no;
+        return flightno;
     }
 
     public void setFlight_no(String flight_no) {
-        this.flight_no = flight_no;
+        this.flightno = flight_no;
     }
 
     public int getPoints_need() {
-        return points_need;
+        return pointsneed;
     }
 
     public void setPoints_need(int points_need) {
-        this.points_need = points_need;
+        this.pointsneed = points_need;
     }
 
     public String getDep_ct() {
-        return dep_ct;
+        return depct;
     }
 
     public void setDep_ct(String dep_ct) {
-        this.dep_ct = dep_ct;
+        this.depct = dep_ct;
     }
 
     public String getArr_ct() {
-        return arr_ct;
+        return arrct;
     }
 
     public void setArr_ct(String arr_ct) {
-        this.arr_ct = arr_ct;
+        this.arrct = arr_ct;
     }
 
     public String getDep_time() {
-        return dep_time;
+        return deptime;
     }
 
     public void setDep_time(String dep_time) {
-        this.dep_time = dep_time;
+        this.deptime = dep_time;
     }
 
     @Override
     public String toString() {
         return "ExchangePoints{" +
-                "flight_no='" + flight_no + '\'' +
-                ", points_need=" + points_need +
-                ", dep_ct='" + dep_ct + '\'' +
-                ", arr_ct='" + arr_ct + '\'' +
-                ", dep_time='" + dep_time + '\'' +
+                "flightno='" + flightno + '\'' +
+                ", pointsneed=" + pointsneed +
+                ", depct='" + depct + '\'' +
+                ", arrct='" + arrct + '\'' +
+                ", deptime='" + deptime + '\'' +
                 '}';
     }
 }
